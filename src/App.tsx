bce9f46a4f7fede460acf53c50d943c565028652
limdumb <dlaruddls@nate.com>
2023-03-28 21:55:06 +0900

    [Feat] Page 설계 및 Route 기본설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import './App.css';
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Search from "./pages/Search";
+import TopRank from "./pages/TopRank";
+import AuthPage from "./pages/AuthPage";
+import ProductList from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-<></>
+    <BrowserRouter>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route>
+          <Route />
+          <Route />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
