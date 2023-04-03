73cfb23edb0f741ac9c0faa078eb00186cfd8d73
limdumb <dlaruddls@nate.com>
2023-04-03 13:51:40 +0900

    [Feat] Main Title Section 구현

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,11 +1,13 @@
 import AsideTab from "../components/common/AsideTab";
 import ProductCard from "../components/common/ProductCard";
+import TitleSection from "../components/common/TitleSection";
 
 export default function ProductList() {
   return (
     <>
       <AsideTab />
       <ProductCard productImage={""} productName={"Gucci 마몽드백"} />
+      <TitleSection/>
     </>
   );
 }
