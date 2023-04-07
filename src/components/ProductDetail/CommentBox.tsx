2f95b92e6b90623be9c95c5fffc953b01d0f3250
limdumb <dlaruddls@nate.com>
2023-04-07 13:04:45 +0900

    [Feat] Comment Box 구현

--- src/components/ProductDetail/CommentBox.tsx
+++ src/components/ProductDetail/CommentBox.tsx
@@ -1,4 +1,50 @@
-export default function CommentBox(){
+import styled from "styled-components";
 
-  return(<></>)
+const CommentBoxContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 20px;
+  height: 90px;
+  background-color: white;
+  border: 1px solid gray;
+
+  & > :last-child {
+    font-weight: 600;
+  }
+`;
+
+const ReviewContents = styled.div`
+  display: flex;
+  & > :first-child {
+    font-size: 14px;
+    font-weight: 600;
+    padding-bottom: 22px;
+  }
+  & > :last-child {
+    width: 1200px;
+    padding-left: 40px;
+    font-size: 15px;
+  }
+`;
+
+export default function CommentBox() {
+  return (
+    <CommentBoxContainer>
+      <ReviewContents>
+        <div>
+          <span>사용자 이름</span>
+        </div>
+        <div>
+          <span>
+            gd
+            리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d리뷰내용d
+          </span>
+        </div>
+      </ReviewContents>
+      <div>
+        <span>3,869,000원</span>
+      </div>
+    </CommentBoxContainer>
+  );
 }
--- src/components/ProductDetail/ProductComment.tsx
+++ src/components/ProductDetail/ProductComment.tsx
@@ -6,26 +6,30 @@ import "./style/productComment.css";
 export default function ProductComment() {
   return (
     <div>
-      <span>리뷰</span>
-      <div>
-        <CommonInput
-          value={""}
-          onChange={() => {}}
-          type={"text"}
-          placeholder={"리뷰를 입력해주세요"}
-          label={"리뷰를 입력해주세요"}
-        />
+      <div className="Product_Review_Write_Container">
         <CommonInput
           value={""}
           onChange={() => {}}
           type={"text"}
           placeholder={"구입하셨던 상품 금액을 입력해주세요"}
-          label={"상품 금액을 입력해주세요"}
+          label={"상품 리뷰를 입력해주세요"}
+          width={"100%"}
+          height={"25px"}
+          border={"1px solid #afafaf"}
+        />
+        <CommonInput
+          value={""}
+          onChange={() => {}}
+          type={"text"}
+          placeholder={"구입하셨던 리뷰를 입력해주세요"}
+          width={"100%"}
+          height={"150px"}
+          border={"1px solid #afafaf"}
         />
       </div>
       <ContourLine />
       <div>
-        <CommentBox/>
+        <CommentBox />
       </div>
     </div>
   );
