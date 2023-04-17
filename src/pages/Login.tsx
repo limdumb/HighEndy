cec0cf14e8e38122d7421c012597f5c92b6b6e27
limdumb <dlaruddls@nate.com>
2023-04-17 11:49:29 +0900

    [Feat] Login 설명글 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -22,32 +22,42 @@ export default function Login() {
         <h2>로그인</h2>
       </div>
       <div className="Input_Wrapper">
-        <CommonInput
-          width={window.innerWidth <= 390 ? "320px" : "490px"}
-          height="34px"
-          radius="3px"
-          name="nickName"
-          value={loginValue.nickName}
-          label={"닉네임"}
-          onChange={(e) => {
-            onLoginValueChanged(e);
-          }}
-          type={"text"}
-          placeholder={"닉네임을 입력해주세요"}
-        />
-        <CommonInput
-          width={window.innerWidth <= 390 ? "320px" : "490px"}
-          height="34px"
-          radius="3px"
-          name="password"
-          value={loginValue.password}
-          label={"비밀번호"}
-          onChange={(e) => {
-            onLoginValueChanged(e);
-          }}
-          type={"password"}
-          placeholder={"비밀번호를 입력해주세요"}
-        />
+        <div className="Input_Container">
+          <CommonInput
+            width={window.innerWidth <= 390 ? "320px" : "490px"}
+            height="34px"
+            radius="3px"
+            name="nickName"
+            value={loginValue.nickName}
+            label={"닉네임"}
+            onChange={(e) => {
+              onLoginValueChanged(e);
+            }}
+            type={"text"}
+            placeholder={"닉네임을 입력해주세요"}
+          />
+          <span>
+            닉네임은 특수문자 미포함 2글자 이상입니다.
+          </span>
+        </div>
+        <div className="Input_Container">
+          <CommonInput
+            width={window.innerWidth <= 390 ? "320px" : "490px"}
+            height="34px"
+            radius="3px"
+            name="password"
+            value={loginValue.password}
+            label={"비밀번호"}
+            onChange={(e) => {
+              onLoginValueChanged(e);
+            }}
+            type={"password"}
+            placeholder={"비밀번호를 입력해주세요"}
+          />
+          <span>
+            비밀번호는 특수문자 미포함 8자 이상입니다.
+          </span>
+        </div>
       </div>
       <div className="SignUp_Button_Wrapper">
         <CustomButton width={"120px"} height={"41px"} contents={"로그인"} />
