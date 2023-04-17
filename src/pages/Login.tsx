2830a3f507e3b9b4b3858ee097dd0f24a584f018
limdumb <dlaruddls@nate.com>
2023-04-17 11:57:06 +0900

    [Feat] 로그인 로직 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from "react";
+import login from "../API/auth/login";
 import CommonInput from "../components/common/CommonInput";
 import CustomButton from "../components/common/CustomButton";
 import { AuthContent } from "./SignUp";
@@ -36,9 +37,7 @@ export default function Login() {
             type={"text"}
             placeholder={"닉네임을 입력해주세요"}
           />
-          <span>
-            닉네임은 특수문자 미포함 2글자 이상입니다.
-          </span>
+          <span>닉네임은 특수문자 미포함 2글자 이상입니다.</span>
         </div>
         <div className="Input_Container">
           <CommonInput
@@ -54,13 +53,19 @@ export default function Login() {
             type={"password"}
             placeholder={"비밀번호를 입력해주세요"}
           />
-          <span>
-            비밀번호는 특수문자 미포함 8자 이상입니다.
-          </span>
+          <span>비밀번호는 특수문자 미포함 8자 이상입니다.</span>
         </div>
       </div>
       <div className="SignUp_Button_Wrapper">
-        <CustomButton width={"120px"} height={"41px"} contents={"로그인"} />
+        <CustomButton
+          width={"120px"}
+          height={"41px"}
+          contents={"로그인"}
+          onClick={() => {
+            //validate는 이전 리뷰확인으로 수정후 적용 예정
+            login({ nickName: loginValue.nickName });
+          }}
+        />
       </div>
     </AuthContent>
   );
