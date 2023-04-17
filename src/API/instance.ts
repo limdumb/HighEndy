435510d07e7469e875d093d9e08a37900dbccd22
limdumb <dlaruddls@nate.com>
2023-04-17 14:35:24 +0900

    [Feat] Axios 공통 에러처리 생성

--- src/API/instance.ts
+++ src/API/instance.ts
@@ -1,9 +1,26 @@
 import axios from "axios";
 
-//Instance는 기본틀 제작 후 추후에 변경 예정
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 export const baseInstance = axios.create({
-  baseURL: "",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+export const authInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.status === 500) {
+      alert("서버 에러가 발생하였습니다. 잠시후 다시 시도해주세요");
+    }
+  }
+);
