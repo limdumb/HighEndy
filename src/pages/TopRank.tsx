9f3eacc479dfe7740825c09e25647dbb81bd1461
limdumb <dlaruddls@nate.com>
2023-04-23 14:08:05 +0900

    [MISC] 불필요한 코드 삭제

--- src/pages/TopRank.tsx
+++ src/pages/TopRank.tsx
@@ -17,19 +17,6 @@ export default function TopRank() {
     productInitialValue
   );
 
-  const test = [
-    "1번상품",
-    "2번상품",
-    "3번상품",
-    "4번상품",
-    "5번상품",
-    "6번상품",
-    "7번상품",
-    "8번상품",
-    "9번상품",
-    "10번상품",
-  ];
-
   return (
     <HomeContainer>
       <ProductListContainer>
